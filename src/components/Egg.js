d98fceba7fb26c3f6605bc1676e624bb644b7bbb
Kinga Kalisz <kingakalisz@gmail.com>
2021-03-29 13:43:30 +0200

    stripes done

---
+++ src/components/Egg.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './css/Egg.css'
+
+const Egg = () => {
+    return (
+        <div className="egg">
+            <div className="stripe"></div>
+            <div className="stripe"></div>
+            <div className="stripe"></div>
+        </div>
+    )
+}
+
+export default Egg
